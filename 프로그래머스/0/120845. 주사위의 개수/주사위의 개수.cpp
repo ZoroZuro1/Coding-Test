c57326a6058e4a297a5bcfd3e1934d696c616f26
JIU <94626295+ZoroZuro1@users.noreply.github.com>
2024-02-10 06:10:38 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cpp
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cpp
@@ -2,16 +2,9 @@
 #include <vector>
 
 using namespace std;
-int maxNum(int size, int n){
-    int i;
-    for(i=1; i*n<=size;){
-        i++;
-    }
-    return i-1;
-}
 
 int solution(vector<int> box, int n) {
     int answer = 0;
-    answer = maxNum(box[0], n) * maxNum(box[1], n) * maxNum(box[2], n);
+    answer = (box[0]/n) * (box[1]/n) * (box[2]/n);
     return answer;
 }
