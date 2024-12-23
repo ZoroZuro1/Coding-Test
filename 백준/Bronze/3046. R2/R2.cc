24d8e5e5deeac8e75d0d2f15a5b8fbcf8ab5a976
JIU <94626295+ZoroZuro1@users.noreply.github.com>
2024-12-23 18:54:06 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int r1, s, r2 = 0;
+
+    cin >> r1 >> s;
+    r2 = s * 2 - r1;
+
+    cout << r2;
+}
