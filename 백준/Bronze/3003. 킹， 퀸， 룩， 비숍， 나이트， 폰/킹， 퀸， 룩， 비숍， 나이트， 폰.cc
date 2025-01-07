b5904b5657b532faa11730dc5adb7cf3c83dc60d
JIU <94626295+ZoroZuro1@users.noreply.github.com>
2025-01-07 18:12:15 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int num = 0;
+    cin >> num;
+    cout << 1 - num << " ";
+    cin >> num;
+    cout << 1 - num << " ";
+    cin >> num;
+    cout << 2 - num << " ";
+    cin >> num;
+    cout << 2 - num << " ";
+    cin >> num;
+    cout << 2 - num << " ";
+    cin >> num;
+    cout << 8 - num;
+}
