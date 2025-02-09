6ac88ff0ffa4d83c1fd6653cecc437758d4410bf
JIU <94626295+ZoroZuro1@users.noreply.github.com>
2025-02-09 14:26:32 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int alpha[27];
+int main(void) 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |";
+    return 0;
+}
