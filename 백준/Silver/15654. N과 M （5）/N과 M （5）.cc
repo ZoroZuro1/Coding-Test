205dec9ca41b298356b7931d3f7ebd503472ce52
JIU <94626295+ZoroZuro1@users.noreply.github.com>
2025-03-11 19:01:08 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int output[8];
+int arr[8];
+bool isused[8];
+
+void func(int cur){ // 현재 선택한 수의 개수
+    if(cur == m){
+        // 저장된 배열 출력 후 return
+        for(int i=0; i<m; i++){
+            cout << output[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=0; i<n; i++){
+        if(!isused[i]){
+            output[cur] = arr[i]; // 차례대로 넣기
+            isused[i] = true;
+            func(cur+1); // 다음 수 고르기
+            isused[i] = false; // 고른 뒤엔 false로 바꿔야 함
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+
+    // 정렬 (오름차순)
+    sort(arr, arr+n, less<>());
+
+    func(0);
+
+    return 0;
+}
