528b241fa84fbf8374884e945ff4571cf6f05e83
JIU <94626295+ZoroZuro1@users.noreply.github.com>
2025-04-27 16:37:16 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.cc
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.cc
@@ -1,28 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int gcd(int a, int b){
+  if(b==0) return a;
+  return gcd(b, a%b);
+}
+
+int lcm(int a, int b){
+  return a/gcd(a,b) * b;
+}
+
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   int arr[5];
-
   for(int i=0; i<5; i++){
     cin >> arr[i];
   }
 
-  // 브루트포스
-  for(int i=4; i<=1000000; i++){
-    int k = 0;
-    for(int j=0; j<5; j++){
-      if(i%arr[j]==0){
-        k++;
-        if(k>=3) {
-          cout << i;
-          return 0;
-        }
+  int ans = 1000000;
+
+  for(int i=0; i<5; i++){
+    for(int j=i+1; j<5; j++){
+      for(int k=j+1; k<5; k++){
+        int temp = lcm(arr[i], arr[j]);
+        temp = lcm(temp, arr[k]);
+        ans = min(ans, temp);
       }
     }
   }
+
+  cout << ans;
+
   return 0;  
 }
